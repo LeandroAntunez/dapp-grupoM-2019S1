daa69184e7d7cf566d9bf2213563a21c5300e599
Leandro Antunez <leandro_antunez@outlook.com>
2019-04-24 16:37:34 -0300

    An created Event has guest.

--- src/main/java/ar/edu/unq/desapp/eventeando/backend/model/Event.java
+++ src/main/java/ar/edu/unq/desapp/eventeando/backend/model/Event.java
@@ -1,12 +1,18 @@
 package ar.edu.unq.desapp.eventeando.backend.model;
 
+import java.util.List;
+
 public class Event {
 
     private User host;
+    private List<User> guestList;
 
-    public Event(User host) {
+    public Event(User host, List<User> guestList) {
         this.setHost(host);
+        this.setGuestList(guestList);
     }
+
+    // Getters && Setters
 
     public void setHost(User host) {
         this.host = host;
@@ -14,5 +20,13 @@ public class Event {
 
     public User getHost() {
         return host;
+    }
+
+    public void setGuestList(List<User> guestList) {
+        this.guestList = guestList;
+    }
+
+    public List<User> getGuestList() {
+        return guestList;
     }
 }
--- src/test/java/ar/edu/unq/desapp/eventeando/backend/model/EventTest.java
+++ src/test/java/ar/edu/unq/desapp/eventeando/backend/model/EventTest.java
@@ -1,19 +1,45 @@
 package ar.edu.unq.desapp.eventeando.backend.model;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 
 public class EventTest {
-	
+
+	private User host;
+	private Event event;
+	private User guest;
+	private List<User> guestList;
+
+	@Before
+	public void setUp(){
+		host = mock(User.class);
+		guest = mock(User.class);
+		guestList = new ArrayList<User>();
+	}
+
+	@After
+	public void tearDown() {
+		guestList.clear();
+	}
+
 	@Test
 	public void createdEventHasHost() {
-		User host = mock(User.class);
-		Event event = new Event(host);
+		event = new Event(host, guestList);
 		Assert.assertEquals(event.getHost(), host);
 	}
 
-
+	@Test
+	public void createdEventHasGuest() {
+		guestList.add(guest);
+		event = new Event(host, guestList);
+		Assert.assertEquals(guestList, event.getGuestList());
+	}
 
 }
