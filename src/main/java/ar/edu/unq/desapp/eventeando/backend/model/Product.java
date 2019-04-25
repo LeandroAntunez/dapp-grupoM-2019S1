6c2602141c7c361318e2f58b4155d859b1489fe5
Leandro Antunez <leandro_antunez@outlook.com>
2019-04-25 15:44:33 -0300

    Product has price.

--- src/main/java/ar/edu/unq/desapp/eventeando/backend/model/Product.java
+++ src/main/java/ar/edu/unq/desapp/eventeando/backend/model/Product.java
@@ -1,10 +1,14 @@
 package ar.edu.unq.desapp.eventeando.backend.model;
 
+import org.joda.money.Money;
+
 public class Product {
     private String name;
+    private Money price;
 
-    public Product(String productName) {
+    public Product(String productName, Money price) {
         this.setName(productName);
+        this.setPrice(price);
     }
 
     private void setName(String name) {
@@ -13,5 +17,13 @@ public class Product {
 
     public String getName() {
         return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    private void setPrice(Money price) {
+        this.price = price;
     }
 }
--- src/test/java/ar/edu/unq/desapp/eventeando/backend/model/ProductTest.java
+++ src/test/java/ar/edu/unq/desapp/eventeando/backend/model/ProductTest.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.desapp.eventeando.backend.model;
 
+import org.joda.money.Money;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,16 +9,25 @@ public class ProductTest {
 
     private Product product;
     private String productName;
+    private Money price;
 
     @Before
     public void setUp(){
         productName = "Water bottle";
+        price = Money.parse("ARS 40.00");
     }
 
     @Test
     public void createdProductHasName(){
-        product = new Product(productName);
+        product = new Product(productName, price);
         Assert.assertEquals(productName, product.getName());
     }
+
+    @Test
+    public void createdProductHasPrice(){
+        product = new Product(productName, price);
+        Assert.assertEquals(product.getPrice(), price);
+    }
+
 
 }
