c4e21f9683bdba10a47b450956daf32250e06550
Leandro Antunez <leandro_antunez@outlook.com>
2019-05-10 15:49:45 -0300

    EventeandoApplication moved to webservice package.

--- src/main/java/ar/edu/unq/desapp/eventeando/backend/webservice/EventeandoApplication.java
+++ src/main/java/ar/edu/unq/desapp/eventeando/backend/webservice/EventeandoApplication.java
@@ -1,4 +1,4 @@
-package ar.edu.unq.desapp.eventeando.backend;
+package ar.edu.unq.desapp.eventeando.backend.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
