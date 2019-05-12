55ddb09eb3dcb189c25c691d20dd7734457d9d71
Leandro Antunez <leandro_antunez@outlook.com>
2019-05-12 01:49:29 -0300

    Refactor: an generic controller added.

---
+++ src/main/java/ar/edu/unq/desapp/eventeando/backend/controller/AbstractController.java
@@ -0,0 +1,51 @@
+package ar.edu.unq.desapp.eventeando.backend.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+public abstract class AbstractController<T> {
+
+    private final JpaRepository<T, Long> repo;
+
+    AbstractController(JpaRepository<T, Long> repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public List<T> getAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable("id") Long id) {
+        return repo.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public T create(@RequestBody T entity) {
+        return repo.save(entity);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity update(@PathVariable("id") Long id, @RequestBody T source) {
+        return repo.findById(id)
+                .map(target -> { BeanUtils.copyProperties(source, target, "id"); return target; })
+                .map(repo::save)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        return repo.findById(id)
+                .map(entity -> { repo.delete(entity); return entity; })
+                .map(t -> ResponseEntity.noContent().build())
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
--- src/main/java/ar/edu/unq/desapp/eventeando/backend/controller/ProductController.java
+++ src/main/java/ar/edu/unq/desapp/eventeando/backend/controller/ProductController.java
@@ -1,60 +1,14 @@
 package ar.edu.unq.desapp.eventeando.backend.controller;
 import ar.edu.unq.desapp.eventeando.backend.model.product.Product;
 import ar.edu.unq.desapp.eventeando.backend.repository.ProductRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/api/v1/product"})
-public class ProductController {
+@RequestMapping({"/product"})
+public class ProductController extends AbstractController<Product>{
 
-    private final ProductRepository repository;
-
-    public ProductController(ProductRepository repository) {
-        this.repository = repository;
+    public ProductController(ProductRepository repository){
+        super(repository);
     }
-
-
-    @GetMapping
-    public List findAll(){
-        return repository.findAll();
-    }
-
-    @GetMapping(path = {"/{id}"})
-    public ResponseEntity<Product> findById(@PathVariable long id){
-        return repository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @PostMapping
-    public Product create(@Valid @RequestBody Product product){
-        return repository.save(product);
-    }
-
-    @PutMapping(value="/{id}")
-    public ResponseEntity<Product> update(@PathVariable("id") long id,
-                                       @RequestBody Product product){
-        return repository.findById(id)
-                .map(record -> {
-                    record.setName(product.getName());
-                    record.setPrice(product.getPrice());
-                    record.setProductCategory(product.getProductCategory());
-                    Product updated = repository.save(record);
-                    return ResponseEntity.ok().body(updated);
-                }).orElse(ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping(path ={"/{id}"})
-    public ResponseEntity<?> delete(@PathVariable("id") long id) {
-        return repository.findById(id)
-                .map(record -> {
-                    repository.deleteById(id);
-                    return ResponseEntity.ok().build();
-                }).orElse(ResponseEntity.notFound().build());
-    }
-
 }
